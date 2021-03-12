a11b4128a7e5c378bd4a011497201146a17e86b8
Rogick Alves Manoel <rogick.manoel@sankhya.com.br>
2021-03-12 10:38:31 -0300

    Finalização do curso

--- src/main/java/br/com/rgk/alura/Produto.java
+++ src/main/java/br/com/rgk/alura/Produto.java
@@ -1,5 +1,31 @@
 package br.com.rgk.alura;
 
-public class Produto {
+import java.math.BigDecimal;
 
+public class Produto {
+	
+	private String nome;
+	private BigDecimal preco;
+	
+	public Produto(String nome, BigDecimal preco) {
+		super();
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
 }
--- src/test/java/br/com/rgk/alura/test/ProdutoTest.java
+++ src/test/java/br/com/rgk/alura/test/ProdutoTest.java
@@ -1,14 +1,21 @@
 package br.com.rgk.alura.test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
 
 import org.junit.Test;
+
+import br.com.rgk.alura.Produto;
 
 public class ProdutoTest {
 
 	@Test
 	public void test() {
-		assertTrue(true);
+		Produto p = new Produto("banana", BigDecimal.TEN);
+		
+		assertEquals(p.getPreco(), BigDecimal.TEN);
+		assertEquals(p.getNome(), "banana");
 	}
 
 }
